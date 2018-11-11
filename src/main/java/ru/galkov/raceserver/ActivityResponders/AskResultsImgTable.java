623ecfb56855fef4a86800be783a8b6f29c0a38d
Vladimir Galkov <s0506777@yandex.ru>
2018-11-11 04:09:53 +0400

    добавлена поддержка яндекс карт и посылки картинок клиенту.

---
+++ src/main/java/ru/galkov/raceserver/ActivityResponders/AskResultsImgTable.java
@@ -0,0 +1,73 @@
+package ru.galkov.raceserver.ActivityResponders;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.json.*;
+import org.springframework.web.bind.annotation.*;
+
+import ru.galkov.raceserver.RaceServerApplication;
+import ru.galkov.raceserver.RaceServerApplication.fieldsJSON;
+import ru.galkov.raceserver.db_access.UsersDAO;
+import ru.galkov.raceserver.db_access.model.Users;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.*;
+
+	@RestController
+	public class AskResultsImgTable extends AskRoot {
+
+		private static final String ASKER = "AskResultsImgTable";
+		private final String resultDirectory = RaceServerApplication.resultDirectory;
+		
+		
+		@RequestMapping(value = "/" + ASKER + "/{inJSON}") 
+		@ResponseBody
+		public String makeAnswer(@PathVariable("inJSON") String inJSON, 
+					final HttpServletRequest request,
+					final HttpServletResponse response) {
+			String exec_level ="Error", exec_login = "nobody",  fileType ="", clientKey = "", imgType="ALL";
+			JSONObject outBoundJSON = new JSONObject();
+			JSONObject inBoundJSON = null;
+
+			try {
+
+					outBoundJSON.put(fieldsJSON.asker.toString(), ASKER);
+					inBoundJSON = new JSONObject(inJSON);
+// http://192.168.1.5:8080/AskResultsImgTable/{"exec_level":"Admin","exec_login":"+79272006026","asker":"AskResultsImgTable","level"="Admin","key":"galkovvladimirandreevich"}
+					//http://192.168.1.5:8080/AskResultsImgTable/{'exec_level':'Admin','exec_login':'+79272006026','asker':'AskResultsImgTable','level'='Admin','key':'galkovvladimirandreevich'}
+					clientKey = inBoundJSON.getString(fieldsJSON.key.toString());		
+					exec_level = inBoundJSON.getString(fieldsJSON.exec_level.toString());
+					exec_login = inBoundJSON.getString(fieldsJSON.exec_login.toString());
+//					imgType = inBoundJSON.getString(fieldsJSON.IMGType.toString());
+									
+					if (serverKey.chkFileKey(clientKey))	{
+			            outBoundJSON.put(fieldsJSON.key.toString(), serverKey.getFileKey());
+						UsersDAO users = new UsersDAO();
+						List<Users> Rows = null;
+			            			    		
+						ByteArrayOutputStream os = new ByteArrayOutputStream();
+		                Graphic image = new Graphic();
+		                ImageIO.write(image.DrawDefault(), "png", os);
+		                
+		                try {
+		                		InputStream fileInputStream = new ByteArrayInputStream(os.toByteArray());
+		                		OutputStream output = response.getOutputStream();
+			                	response.reset();
+			                	response.setContentType("image/jpeg");
+					            IOUtils.copyLarge(fileInputStream, output);
+					            output.flush();;
+				            } 
+			            catch (IOException e)	{	e.getMessage();  }
+				        catch (Exception e)		{	e.getMessage();  }
+					}
+					else {	outBoundJSON.put(fieldsJSON.error.toString(), clientKey + "- не верный!");	}
+			} 
+			catch (JSONException | IOException e) { 	e.printStackTrace();logger.error(ASKER + "Ошибка формата протокола. Не отработал");}
+
+			new WriteLog(ASKER,inBoundJSON, outBoundJSON, exec_login, exec_level);
+			return outBoundJSON.toString();
+		}
+		
+}
---
+++ src/main/java/ru/galkov/raceserver/ActivityResponders/Graphic.java
@@ -0,0 +1,41 @@
+package ru.galkov.raceserver.ActivityResponders;
+
+	import java.awt.Color;
+	import java.awt.Font;
+	import java.awt.Graphics;
+	import java.awt.image.BufferedImage;
+
+	public class Graphic {
+		public void Close(){	}
+		
+		Graphic(){	}
+		
+		public BufferedImage DrawDefault() {
+			// странная координатная сетка (не понятные мне поправки, дающие правильное изображение)
+			int ImageWidth = 520;
+			int ImageHeight = 220;
+			int ramka = 3;
+	        BufferedImage b = new BufferedImage(ImageWidth, ImageHeight, BufferedImage.TYPE_BYTE_INDEXED);
+	        Graphics gr = b.getGraphics();
+	        gr.setColor(Color.white);
+	        gr.fillRect(0, 0, b.getWidth(), b.getHeight());
+	        
+	        gr.setColor(Color.black);
+	        gr.drawRect(ramka, ramka, ImageWidth-2*ramka, ImageHeight-2*ramka);
+	        
+	        Font currentFont = gr.getFont();
+		    Font newFont = currentFont.deriveFont(currentFont.getSize()-4 );
+		    gr.setFont(newFont);
+		    gr.drawString("График результатов участников текущего старта.",(ImageWidth/2)-80,20);
+
+	        gr.drawRect(ramka, 50, ImageWidth-2*ramka, ImageHeight-2*ramka);
+	        
+	        // draw grid
+		    gr.setColor(Color.gray);        
+	        for (int i = ramka; i<ImageWidth; i=i+(ImageWidth/10)) { gr.drawLine(i, 50, i, ImageHeight-2*ramka); }
+	        for (int i = ramka; i<ImageHeight; i=i+(ImageHeight/5)) { gr.drawLine(ramka, 50+i, ImageWidth-2*ramka, 50+i);   }
+	        
+			return b;	
+		}
+
+	}
